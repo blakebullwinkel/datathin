7083910424378243c365a304ce2c2a3ca32918e7
blakebullwinkel <blakebullwinkel@gmail.com>
2024-02-18 22:15:12 -0800

    cleaned up input validations

--- datathin/thinning.py
+++ datathin/thinning.py
@@ -1,108 +1,131 @@
-import datathin.helpers as helpers
 import warnings
 import numpy as np
+from datathin.helpers import *
 
-from typing import Union, List
-from pydantic import BaseModel, validator
-
-class DataThinner(BaseModel):
-    data: Union[List[Union[int, float]], np.ndarray]
-    family: str
-    K: int = 2
-    epsilon: Union[List[float], np.ndarray, None] = None
-    arg: Union[int, float, List[Union[int, float]], np.ndarray, None] = None
 
 def datathin(data, family, K=2, epsilon=None, arg=None) -> np.ndarray:
-    data = np.array(data)
-    data_shape = data.shape
-    # TODO: preprocess arg in a better way than this
-    if isinstance(arg, (int, float)):
-        arg_arr = np.array([arg])
+
+    # Validate inputs
+    if isinstance(data, list):
+        data = np.array(data)
+    elif isinstance(data, np.ndarray):
+        pass
     else:
-        arg_arr = np.array(arg)
-    arg_shape = arg_arr.shape
+        raise ValueError("`data` must be either a list or numpy array.")
+    data_shape = data.shape
 
     if family in ["poisson", "exponential", "chi-squared", "scaled-uniform"]:
         if arg is not None:
             raise warnings.warn(
-                f"Extra parameter provided was not used in {family} thinning.", 
-                UserWarning
+                f"Extra parameter provided was not used in {family} thinning.",
+                UserWarning,
             )
     else:
         if arg is None:
             raise ValueError("Extra parameter is missing.")
-        elif not isinstance(arg, (int, float)): # TODO: figure out how to check this properly
-            raise ValueError("Non-numeric parameter provided.")
-        else:
-            if family in ["mvnormal", "mvgaussian"]:
-                if len(arg_shape) == 2:
-                    # TODO: check this condition
-                    if np.any(arg_shape != [data_shape[1], data_shape[1]]):
-                        raise ValueError("Incorrect dimensions for multivariate normal covariance matrices.")
-                elif len(arg_shape) == 3:
-                    if np.any(arg_shape != [data_shape[1], data_shape[1], data_shape[1]]):
-                        raise ValueError("Incorrect dimensions for multivariate normal covariance matrices.")
-            elif family == "multinomial":
-                if len(arg_arr) not in [1, data_shape[0]]:
-                    raise ValueError("Incorrect dimensions for multinomial trials parameter.")
+
+        if not isinstance(arg, np.ndarray):
+            if isinstance(arg, (int, float)):
+                if family == "multinomial":
+                    arg = np.repeat(arg, data_shape[0])
+                else:
+                    arg = arg * np.ones(data_shape)
+            elif isinstance(arg, list):
+                arg = np.array(arg)
             else:
-                if len(arg_arr) > 1:
-                    if np.any(arg_shape != data_shape):
-                        raise ValueError("If `arg` is not a scalar, its dimensions must match those of `data`.")
-                    
-    if family in ["poisson", "negative binomial", "normal", "gaussian", "mvnormal",
-                  "mvgaussian", "binomial", "multinomial", "exponential", "gamma"]:
+                raise ValueError(
+                    "`arg` must be either a numerical value or array of numerical values."
+                )
+        arg_shape = arg.shape
+
+        if family in ["mvnormal", "mvgaussian"]:
+            if len(arg_shape) == 2:
+                # TODO: check this condition
+                if np.any(arg_shape != [data_shape[1], data_shape[1]]):
+                    raise ValueError(
+                        "Incorrect dimensions for multivariate normal covariance matrices."
+                    )
+            elif len(arg_shape) == 3:
+                if np.any(arg_shape != [data_shape[0], data_shape[1], data_shape[1]]):
+                    raise ValueError(
+                        "Incorrect dimensions for multivariate normal covariance matrices."
+                    )
+        elif family == "multinomial":
+            if len(arg) not in [1, data_shape[0]]:
+                raise ValueError(
+                    "Incorrect dimensions for multinomial trials parameter."
+                )
+        else:
+            if len(arg) > 1:
+                if np.any(arg_shape != data_shape):
+                    raise ValueError(
+                        "If `arg` is not a scalar, its dimensions must match those of `data`."
+                    )
+
+    if family in [
+        "poisson",
+        "negative binomial",
+        "normal",
+        "gaussian",
+        "mvnormal",
+        "mvgaussian",
+        "binomial",
+        "multinomial",
+        "exponential",
+        "gamma",
+    ]:
         if epsilon is None:
-            epsilon = np.repeat(1/K, K)
+            epsilon = np.repeat(1 / K, K)
         else:
             if np.sum(epsilon) != 1:
                 raise ValueError("`epsilon` does not sum to 1.")
             if len(epsilon) != K:
-                raise warnings.warn("`K` parameter will be ignored in favor of the length of `epsilon`.")
-            
-    if len(arg_arr) == 1:
-        if family == "multinomial":
-            arg = np.repeat(arg, data_shape[0])
-        else:
-            arg = arg*np.ones(data_shape)
+                raise warnings.warn(
+                    "`K` parameter will be ignored in favor of the length of `epsilon`."
+                )
 
     if family == "poisson":
-        X = helpers.poisthin(data, epsilon)
+        X = poisthin(data, epsilon)
     elif family == "negative binomial":
-        X = helpers.nbthin(data, epsilon, arg)
+        X = nbthin(data, epsilon, arg)
     elif family in ["normal", "gaussian"]:
-        X = helpers.normthin(data, epsilon, arg)
+        X = normthin(data, epsilon, arg)
     elif family in ["normal-variance", "gaussian-variance"]:
-        X = helpers.normvarthin(data, arg, K)
+        X = normvarthin(data, arg, K)
     elif family in ["mvnormal", "mvgaussian"]:
-        X = helpers.mvnormthin(data, epsilon, arg)
+        X = mvnormthin(data, epsilon, arg)
     elif family == "binomial":
-        X = helpers.binomthin(data, epsilon, arg)
+        X = binomthin(data, epsilon, arg)
     elif family == "multinomial":
-        X = helpers.multinomthin(data, epsilon, arg)
+        X = multinomthin(data, epsilon, arg)
     elif family == "exponential":
-        X = helpers.gammathin(data, epsilon, np.ones(data_shape))
+        X = gammathin(data, epsilon, np.ones(data_shape))
     elif family == "gamma":
-        X = helpers.gammathin(data, epsilon, arg)
+        X = gammathin(data, epsilon, arg)
     elif family == "chi-squared":
-        X = helpers.chisqthin(data, K)
+        X = chisqthin(data, K)
     elif family == "gamma-weibull":
-        X = helpers.gammaweibullthin(data, K, arg)
+        X = gammaweibullthin(data, K, arg)
     elif family == "weibull":
-        X = helpers.weibullthin(data, arg, K)
+        X = weibullthin(data, arg, K)
     elif family == "pareto":
-        X = helpers.paretothin(data, arg, K)
+        X = paretothin(data, arg, K)
     elif family == "shifted-exponential":
-        X = helpers.shiftexpthin(data, arg, K)
+        X = shiftexpthin(data, arg, K)
     elif family == "scaled-uniform":
-        X = helpers.scaledbetathin(data, np.ones(data_shape), K)
+        X = scaledbetathin(data, np.ones(data_shape), K)
     elif family == "scaled-beta":
-        X = helpers.scaledbetathin(data, arg, K)
+        X = scaledbetathin(data, arg, K)
     else:
         raise ValueError(f"Family `{family}` not recognized.")
-    
+
     return X
+
 
 if __name__ == "__main__":
 
-    pass
+    data = np.random.poisson(lam=7, size=(100000, 1))
+    print(data.shape)
+
+    X = datathin(data, family="poisson", K=2)
+    print(X.shape)
